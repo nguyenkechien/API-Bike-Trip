7c62fe6571376103ca51a4a65a9249c8ce4a614d
Chiến Nguyễn Kế <nguyenkechien@outlook.com.vn>
2019-11-27 13:35:12 +0700

    update upload

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const usersRoutes = require("./db/route/users.route");
 const catalogRoutes = require("./db/route/catalog.route");
 const contactRoutes = require("./db/route/contact.route");
 const config = require("config");
-const multer = require('multer');
-const upload = multer({dest: __dirname + '/uploads/images'});
-
+const multer = require("multer");
 
 //use config module to get the privatekey, if no private key set, end the application
 if (!config.get("privatekey")) {
@@ -51,22 +49,43 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use("/api/users", usersRoutes);
 app.use("/api/catalog", catalogRoutes);
 app.use("/api/contact", contactRoutes);
 app.use("/api/products", productsRoutes);
 
-
 // Defined
-app.post('/api/uploadimage', upload.single('photo'), (req, res) => {
-  if(req.file) {
-      res.json(req.file);
+// app.post('/api/uploadimage', upload.single('photo'), (req, res) => {
+//   if(req.file) {
+//       res.json(req.file);
+//   }
+//   else throw 'error';
+// });
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
   }
-  else throw 'error';
 });
 
+// var upload = multer({ storage: storage }).single("images");
+const upload = multer({ dest: __dirname + "/uploads/images" }).single("images");
+
+app.post("/api/uploadimage", function(req, res) {
+  upload(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+    return res.status(200).send(req.file);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
